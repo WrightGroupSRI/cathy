5ede7a4c8ecbe2279ab921e7885a645956d9be3f
Sebastian Ferguson <ferguson@sri.utoronto.ca>
2020-04-29 14:22:04 -0400

    First pass at being able to peek directories

---
+++ cathy/art.py
@@ -0,0 +1,50 @@
+
+import numpy
+
+from matplotlib import pyplot
+
+import catheter_utils.projections
+
+
+class ProjectionsAnimator:
+
+    def __init__(self, projections_meta, projections_raw):
+        self.meta = projections_meta
+        self.projections = [catheter_utils.projections.reconstruct(raw) for raw in projections_raw]
+        self.signal_max = max(numpy.amax(p) for p in self.projections)
+        self.signal_min = min(numpy.amin(p) for p in self.projections)
+        max_fov = projections_meta["fov"].max()
+        self.domain_max = max_fov/2.0
+        self.domain_min = -self.domain_max
+
+    def _pick(self, i, axis):
+        fov = self.meta["fov"][i]
+        fs = self.projections[i][axis]
+        xs = numpy.linspace(-fov / 2, fov / 2, len(fs))
+        return xs, fs
+
+    def plot(self, i, axis, ax=None, plot_args=None, plot_kwargs=None):
+        if not plot_args:
+            plot_args = []
+        if not plot_kwargs:
+            plot_kwargs = {}
+
+        xs, fs = self._pick(i, axis)
+        if ax:
+            return ax.plot(xs, fs, *plot_args, **plot_kwargs)
+        else:
+            return pyplot.plot(xs, fs, *plot_args, **plot_kwargs)
+
+    def animate(self, i, axis, plot=None):
+        xs, fs = self._pick(i, axis)
+        if plot:
+            plot.set_data(xs, fs)
+
+    @property
+    def xlim(self):
+        return self.domain_min, self.domain_max
+
+    @property
+    def ylim(self):
+        return self.signal_min, self.signal_max
+
--- cathy/cli.py
+++ cathy/cli.py
@@ -1,18 +1,24 @@
 """Command Line Interface for cathy."""
 
-import catheter_ukf
-import catheter_utils.cathcoords
-import catheter_utils.projections
 import click
 import click_log
 import datetime
 import enlighten
+import functools
 import logging
 import numpy
 import os
+import random
+import re
 
-from matplotlib import animation
-from matplotlib import pyplot
+from matplotlib import animation, pyplot
+from pathlib import Path
+
+import catheter_ukf
+import catheter_utils.cathcoords
+import catheter_utils.projections
+
+from . import art
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -32,6 +38,51 @@ class ExistingDirectory(click.Path):
         kwargs.update(file_okay=False, dir_okay=True, exists=True)
         super().__init__(*args, **kwargs)
 
+
+class Ints(click.ParamType):
+    """Options strings will be an equals sign followed by one or more
+    non-negative integers, separated by commas and dashes. Dashes indicate
+    ranges (e.g., "1-3" -> [1, 2, 3]), and commas
+    separate values and ranges.
+    """
+
+    name = "INTs"
+
+    def convert(self, value, param, ctx):
+        if value is None or len(value) == 0:
+            return None
+        if value[0] == '=':
+            value = value[1:-1]
+
+        range_re = re.compile(r"(\d+)-(\d+)")
+        values = []
+        for part in value.split(","):
+            try:
+                values.append(int(part))
+            except ValueError:
+                range_match = range_re.match(part)
+                if range_match:
+                    values.extend(range(int(range_match.group(1)), 1 + int(range_match.group(2))))
+                else:
+                    self.fail("unsupported option format: {}".format(value), param, ctx)
+
+        return sorted(values)
+
+
+class ChoiceOrInts(Ints):
+    def __init__(self, choices):
+        self.choices = set([choice.lower() for choice in choices])
+
+    @property
+    def name(self):
+        return "[{}|{}]".format("|".join(self.choices), super().name)
+
+    def convert(self, value, param, ctx):
+        lvalue = value.lower()
+        if lvalue in self.choices:
+            return lvalue
+        else:
+            return super().convert(value, param, ctx)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # cathy root
@@ -96,7 +147,7 @@ def apply_ukf(src, dst, distal, proximal):
     number_of_recordings = len(src_info)
 
     # TODO Should find a way to pass these params. Catheters may have slightly
-    # different geometry. Also ukf.Q
+    #   different geometry. Also ukf.Q
     ukf = catheter_ukf.UKF()
 
     with enlighten.get_manager() as manager:
@@ -173,7 +224,32 @@ def apply_ukf(src, dst, distal, proximal):
 def info(path):
     """Summarize the contents of a projection file."""
     # maybe we want info from another kind of file in the future
-    _proj_info(path)
+    if Path(path).is_dir():
+        _proj_dir_info(path)
+    else:
+        _proj_info(path)
+
+
+def _proj_dir_info(path):
+    discoveries, unknown = catheter_utils.projections.discover_raw(path)
+    print("This directory contains:")
+    if not discoveries.empty:
+        nsri = discoveries["recording"][discoveries["scheme"] == "sri"].nunique()
+        if nsri > 0:
+            sdf = discoveries[discoveries["scheme"] == "sri"]
+            print("  {} 'sri' sampled recordings".format(nsri))
+            print("     from coils: {}".format(sdf["coil"].unique()))
+
+        nhadamard = discoveries["recording"][discoveries["scheme"] == "fh"].nunique()
+        if nhadamard > 0:
+            hdf = discoveries[discoveries["scheme"] == "fh"]
+            print("  {} 'fh' sampled recordings".format(nhadamard))
+            print("     from coils: {}".format(hdf["coil"].unique()))
+            print("   with dithers: {}".format(hdf["dither"].unique()))
+
+    nunknown = len(unknown)
+    if nunknown > 0:
+        print("  {} unknown .projections files".format(nunknown))
 
 
 def _proj_info(path):
@@ -184,10 +260,10 @@ def _proj_info(path):
         pix = len(raw[0][0])
         if meta["fov"].nunique() == 1:
             fov = meta["fov"][0]
-            fov = "{} mm (resolution {} mm)".format(fov, fov/pix)
+            fov = "{} mm (resolution {:.2f} mm)".format(fov, fov/pix)
         else:
             min_fov, max_fov = meta["fov"].min(), meta["fov"].max()
-            fov = "{} - {} (resolution {} - {} mm)".format(min_fov, max_fov, min_fov/pix, max_fov/pix)
+            fov = "{} - {} (resolution {:.2f} - {:.2f} mm)".format(min_fov, max_fov, min_fov/pix, max_fov/pix)
     else:
         fov = "unknown fov"
 
@@ -209,11 +285,98 @@ def _proj_info(path):
 
 @cathy.command()
 @click.argument("path", type=click.Path(exists=True))
+@click.option("-s", "--scheme", type=click.Choice(["sri", "fh"]), help="Select scheme for visualization.")
+@click.option("-c", "--coil", type=Ints(), help="Select coil(s) for visualization.")
+@click.option("-a", "--axis", type=Ints(), help="Select axis/axes for visualization.")
+@click.option("-d", "--dither", type=Ints(), help="Select dither(s) for visualization")
+@click.option("-r", "--recording", type=Ints(), help="Select recording(s) for visualization.")
+@click.option("-p", "--pick", type=click.Choice(["rand", "last"]),
+              help="How pick a single recording from the final query.")
 @click_log.simple_verbosity_option()
-def peek(path):
-    """Display the contents of a projection file."""
-    # maybe we want to peek into another kind of file in the future
-    _proj_peek(path)
+def peek(path, pick=None, **kwargs):
+    """Visualize catheter projections.
+
+    If PATH is a directory, select relevant projections from available raw files to visualize.
+    If PATH is a .projections file, display the contents of the file.
+    """
+    if Path(path).is_dir():
+        _proj_dir_peek(path, pick=pick, query=kwargs)
+    else:
+        _proj_peek(path)
+
+
+def _proj_dir_peek(path, *, query, pick):
+    """Peek a .projections directory. Look at available data and select"""
+    if pick is None:
+        pick = "rand"
+
+    # find all available data in the given directory
+    discoveries, unknown = catheter_utils.projections.discover_raw(path)
+
+    # filter data based on user's optional query params
+    query = [discoveries[k].isin(v) for k, v in query.items() if v]
+    if len(query) == 0:
+        selection = discoveries
+    elif len(query) == 1:
+        selection = discoveries[query[0]]
+    else:
+        selection = discoveries[functools.reduce(lambda x, y: x & y, query)]
+
+    # narrow selection to a single visualization
+    recordings = sorted(selection["recording"].unique())
+    if pick == "rand":
+        selected_recording = random.choice(recordings)
+    else:
+        selected_recording = recordings[-1]
+
+    selection = selection[selection["recording"] == selected_recording]
+
+    coils = sorted(selection["coil"].unique())
+    axes = sorted(selection["axis"].unique())
+    filenames = sorted(selection["filename"].unique())
+
+    number_of_frames = None
+    filename_to_artist = {}
+    for filename in filenames:
+        meta, raw, _, _ = catheter_utils.projections.read_raw(filename)
+        artist = art.ProjectionsAnimator(meta, raw)
+        if number_of_frames:
+            number_of_frames = min(number_of_frames, len(raw))
+        else:
+            number_of_frames = len(raw)
+        filename_to_artist[filename] = artist
+
+    fig, axs = pyplot.subplots(len(axes), figsize=[9, 6])
+    if len(axes) > 1:
+        axis_map = {a: axs[i] for i, a in enumerate(axes)}
+    else:
+        axis_map = {axes[0]: axs}
+
+    plot_keys = []
+    for row in selection.itertuples():
+        ax = axis_map[row.axis]
+        artist = filename_to_artist[row.filename]
+        p, = artist.plot(0, row.index, ax=ax)
+        plot_keys.append((artist, row.index, p))
+
+    def init():
+        pass
+
+    def animate(i):
+        for artist, index, p in plot_keys:
+            artist.animate(i, index, plot=p)
+
+    a = animation.FuncAnimation(
+        fig,
+        animate,
+        init_func=init,
+        frames=number_of_frames,
+        interval=10,
+        repeat=True
+    )
+
+    pyplot.show()
+    pyplot.close(fig)
 
 
 def _proj_peek(path):
@@ -222,6 +385,40 @@ def _proj_peek(path):
     if len(raw) == 0 or len(raw[0]) == 0:
         logger.info("no projections found in the file")
         return
+
+    artist = art.ProjectionsAnimator(meta, raw)
+
+    if "timestamp" in meta.columns:
+        interval = numpy.mean(numpy.diff(meta["timestamp"]))
+    else:
+        # if we don't have timestamps assume we are running at approx 24 fps
+        # (for 3 readouts). This won't be real if the readouts are stored
+        # in multiple files (e.g., the FH case) but what can we do?
+        interval = (1000/24) * (len(raw[0])/3)
+
+    fig = pyplot.figure(figsize=[9, 6])
+    ax = pyplot.axes(xlim=artist.xlim, ylim=artist.ylim)
+
+    plots = []
+    for j in range(len(raw[0])):
+        p, = artist.plot(0, j, ax=ax, plot_args=['-*'])
+        plots.append(p)
+
+    def init():
+        pass
+
+    def animate(i):
+        for k, plot in enumerate(plots):
+            artist.animate(i, k, plot=plot)
+
+    a = animation.FuncAnimation(fig, animate, init_func=init, frames=len(raw), interval=interval, repeat=True)
+    pyplot.show()
+    pyplot.close(fig)
+
+
+def _plot_projs(subplots):
+
+    fig, ax = pyplot.subplots(1, 1, sharex="all", sharey="all", figsize=[9, 6])
 
     def pick(i):
         fov = meta["fov"][i]
@@ -236,9 +433,9 @@ def _proj_peek(path):
         # if we don't have timestamps assume we are running at approx 24 fps
         # (for 3 readouts). This won't be real if the readouts are stored
         # in multiple files (e.g., the FH case) but what can we do?
-        interval = (1000/24) * (len(f0)/3)
+        interval = (1000 / 24) * (len(f0) / 3)
 
-    fig = pyplot.figure(figsize=[15, 10])
+    fig = pyplot.figure(figsize=[9, 6])
     ax = pyplot.axes(xlim=(x0[0], x0[-1]), ylim=(0, numpy.max(f0)))
 
     plots = []
@@ -257,6 +454,29 @@ def _proj_peek(path):
     a = animation.FuncAnimation(fig, animate, init_func=init, frames=len(raw), interval=interval, repeat=True)
     pyplot.show()
     pyplot.close(fig)
+
+    # subplots = [
+    #    { "name": str,
+    #      "data": [
+    #           { "name": str, "raw": rawdata }
+    #      ]
+    #    }
+    # ]
+    #
+    # subplot name {} axis
+    # subplot data seq { data seq name }
+    #
+
+    pass
+
+
+@cathy.command()
+@click.argument("path", type=click.Path(exists=True))
+@click_log.simple_verbosity_option()
+def localize(path):
+    toc = catheter_utils.projections.discover_raw(path)
+    recordings = sorted(toc.recording.unique())
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # cathy build-resp
